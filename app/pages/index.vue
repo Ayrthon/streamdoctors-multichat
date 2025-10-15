162b4189ceae43d90ced585634b81098cde7991d
Ayrthon <a.sadikrama@gmail.com>
2025-10-15 16:09:38 +0200

    fixed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <v-main class="d-flex align-center justify-center" style="height: 100vh">
-      <v-container class="d-flex flex-column justify-center" width="400">
+      <!-- <v-container class="d-flex flex-column justify-center" width="400">
         <img class="pb-5" src="/images/cropped-streamdoctors-logo_wit.png" />
         <v-btn color="primary" to="/projects">Login</v-btn>
-      </v-container>
+      </v-container> -->
 
       <div class="flex min-h-screen flex-col items-center justify-center">
         <h1 class="mb-4 text-xl">Login</h1>
