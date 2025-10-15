e62bd533fe07813e23d1cafb43e0306d15ec21bd
Ayrthon <a.sadikrama@gmail.com>
2025-10-15 15:27:14 +0200

    switched to js and added auth

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,14 +5,38 @@
         <img class="pb-5" src="/images/cropped-streamdoctors-logo_wit.png" />
         <v-btn color="primary" to="/projects">Login</v-btn>
       </v-container>
+
+      <div class="flex min-h-screen flex-col items-center justify-center">
+        <h1 class="mb-4 text-xl">Login</h1>
+        <input v-model="email" placeholder="Email" class="mb-2 border p-2" />
+        <input v-model="password" placeholder="Password" type="password" class="mb-2 border p-2" />
+        <button @click="login" class="rounded bg-blue-500 px-4 py-2 text-white">Login</button>
+      </div>
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
+<script setup>
 definePageMeta({
   layout: 'login',
 })
+</script>
+
+<script>
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+const { $auth } = useNuxtApp()
+await signInWithEmailAndPassword($auth, email.value, password.value)
+
+async function login() {
+  try {
+    await signInWithEmailAndPassword($auth, email.value, password.value)
+    alert('Logged in!')
+  } catch (e) {
+    console.error(e)
+    alert('Login failed!')
+  }
+}
 </script>
 
 <style></style>
