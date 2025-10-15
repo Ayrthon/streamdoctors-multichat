e5095e67f1b173c0ce51c1be731fd2af4e721c73
Ayrthon <a.sadikrama@gmail.com>
2025-10-15 14:07:04 +0200

    github firebase install

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,6 +2,7 @@
   <v-layout class="rounded rounded-md border">
     <v-app-bar elevation="0">
       <img class="pa-5" src="/images/cropped-streamdoctors-logo_wit.png" width="240" />
+      <div class="mt-2">MultiChat v0.2</div>
     </v-app-bar>
     <v-navigation-drawer expand-on-hover permanent rail>
       <v-list>
@@ -15,11 +16,19 @@
       <v-divider></v-divider>
 
       <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-folder" title="Projects" value="myfiles"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-folder"
+          title="Projects"
+          value="projects"
+          to="/projects"
+        ></v-list-item>
         <v-list-item prepend-icon="mdi-account-multiple" title="Users" value="shared"></v-list-item>
         <v-list-item prepend-icon="mdi-cog" title="Settings" value="starred"></v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-main>
+      <NuxtPage />
+    </v-main>
     <v-main class="d-flex align-center justify-center"></v-main>
   </v-layout>
 </template>
---
+++ app/pages/projects/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="pa-4">
+    <div class="pb-4">
+      <v-toolbar>
+        <v-toolbar-title text="Projects"></v-toolbar-title>
+        <template v-slot:append>
+          <v-btn icon="mdi-plus"></v-btn>
+        </template>
+      </v-toolbar>
+    </div>
+
+    <div class="d-flex ga-4">
+      <v-card
+        elevation="16"
+        variant="tonal"
+        width="350"
+        class="d-flex flex-column justify-space-between"
+      >
+        <v-card-title>Samsung Hangout</v-card-title>
+        <v-card-text>
+          <v-icon color="red">mdi-youtube</v-icon> SamsungNederland
+          <v-chip color="success">connected <span class="status-dot"></span></v-chip><br />
+          <v-icon color="red">mdi-youtube</v-icon> SamsungBelgium
+          <v-chip color="success">connected <span class="status-dot"></span></v-chip><br />
+          <v-icon>mdi-alpha-t</v-icon> SamsungNederland <v-chip color="error">disconnected</v-chip
+          ><br />
+          <v-icon>mdi-alpha-t</v-icon> SamsungBelgium
+          <v-chip color="error">disconnected</v-chip>
+        </v-card-text>
+        <v-card-actions class="mt-auto">
+          <v-btn
+            append-icon="mdi-chevron-right"
+            color="primary"
+            text="Open project"
+            variant="outlined"
+            block
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <v-card
+        elevation="16"
+        variant="tonal"
+        width="350"
+        class="d-flex flex-column justify-space-between"
+      >
+        <v-card-title>HEMA</v-card-title>
+        <v-card-text>
+          <v-icon color="red">mdi-youtube</v-icon> Hema
+          <v-chip color="success">connected <span class="status-dot"></span></v-chip><br />
+          <v-icon>mdi-alpha-t</v-icon> Hema
+          <v-chip color="success">connected <span class="status-dot"></span></v-chip><br />
+          <v-icon color="purple">mdi-instagram</v-icon> Hema
+          <v-chip color="error">disconnected</v-chip>
+        </v-card-text>
+        <v-card-actions class="mt-auto">
+          <v-btn
+            append-icon="mdi-chevron-right"
+            color="primary"
+            text="Open project"
+            variant="outlined"
+            block
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.status-dot {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #22c55e;
+  margin-left: 8px;
+  box-shadow: 0 0 0 0 rgba(34, 197, 94, 0.7);
+  animation: pulse 1.5s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(0.9);
+    box-shadow: 0 0 0 0 rgba(34, 197, 94, 0.7);
+  }
+  70% {
+    transform: scale(1);
+    box-shadow: 0 0 0 8px rgba(34, 197, 94, 0);
+  }
+  100% {
+    transform: scale(0.9);
+    box-shadow: 0 0 0 0 rgba(34, 197, 94, 0);
+  }
+}
+</style>
